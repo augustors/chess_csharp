f6a62086ae6ef46317957d9519864ad9e871b2d9
Augustors <augusto7410@gmail.com>
2022-05-22 17:51:08 -0300

    Convertendo a posição da matriz para xadrez

--- Chess_Csharp/Program.cs
+++ Chess_Csharp/Program.cs
@@ -8,21 +8,10 @@ namespace Chess_Csharp
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.ToPosicao());
 
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 7));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-
-                Tela.ImprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            Console.ReadLine();
         }
     }
 }
