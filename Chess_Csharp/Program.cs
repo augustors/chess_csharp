381739462bba90a2054f513f9997afdb9e200f26
Augustors <augusto7410@gmail.com>
2022-05-22 16:30:28 -0300

    Implementando o Tabuleiro

--- Chess_Csharp/Program.cs
+++ Chess_Csharp/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace Chess_Csharp
 {
@@ -7,11 +7,9 @@ namespace Chess_Csharp
     {
         static void Main(string[] args)
         {
-            Posicao P;
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.ReadLine();
 
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + P);
         }
     }
 }
