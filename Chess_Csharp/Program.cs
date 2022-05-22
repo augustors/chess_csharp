275433bc8fe8197828f9b718743e98e86a7fe973
Augustors <augusto7410@gmail.com>
2022-05-22 18:01:42 -0300

    Melhorando a Implementação do Método Imprimir Tabuleiro

--- Chess_Csharp/Program.cs
+++ Chess_Csharp/Program.cs
@@ -8,10 +8,21 @@ namespace Chess_Csharp
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.ToPosicao());
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                Tela.ImprimirTabuleiro(tab);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
